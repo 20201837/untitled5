0d2a285106710658a7cb9eb2f8abc17346adc082
20201837 <2848718426>
2021-05-21 19:34:11 +0800

    Initial commit

---
+++ src/gui/FrameDemo.java
@@ -0,0 +1,13 @@
+package gui;
+import gui.sin.MyFrameSin;
+import javax.swing.*;
+public class FrameDemo {
+    public static void main(String args[]){
+      //MyFrameDate1 frame1=new MyFrameDate1("第一个窗口");
+     // frame1.showTime();
+       // MyFrameDate frame2=new MyFrameDate("第二个窗口");
+       // frame2.showTime();
+        MyFrameSin frameSin=new MyFrameSin("正弦曲线");
+
+    }
+}
